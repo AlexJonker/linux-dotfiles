fcc1b876b03da467457c71c25a5a39e618ef7352
Alex Jonker <alex.jonker2008@gmail.com>
2025-07-01 17:58:02 +0200

    idk

--- install.sh
+++ install.sh
@@ -47,6 +47,7 @@ then
     gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3-dark
     gsettings set org.gnome.desktop.interface icon-theme Tela
     gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
+    gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
     # kitty as default terminal
     gsettings set org.cinnamon.desktop.default-applications.terminal exec kitty
