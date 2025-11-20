229e2c7c78395e10d2903f348ac2f7cf2bbf3ac3
Alex Jonker <alex.jonker2008@gmail.com>
2025-11-20 23:30:45 +0100

    Refactor: remove old user options file migration logic

--- .config/ignis/user_options.py
+++ .config/ignis/user_options.py
@@ -3,25 +3,16 @@ from ignis.options_manager import OptionsGroup, OptionsManager
 from ignis import DATA_DIR, CACHE_DIR  # type: ignore
 
 USER_OPTIONS_FILE = f"{DATA_DIR}/user_options.json"
-OLD_USER_OPTIONS_FILE = f"{CACHE_DIR}/user_options.json"
 
 
 # FIXME: remove someday
 def _migrate_old_options_file() -> None:
-    with open(OLD_USER_OPTIONS_FILE) as f:
-        data = f.read()
-
     with open(USER_OPTIONS_FILE, "w") as f:
         f.write(data)
 
 
 class UserOptions(OptionsManager):
     def __init__(self):
-        if not os.path.exists(USER_OPTIONS_FILE) and os.path.exists(
-            OLD_USER_OPTIONS_FILE
-        ):
-            _migrate_old_options_file()
-
         try:
             super().__init__(file=USER_OPTIONS_FILE)
         except FileNotFoundError:
