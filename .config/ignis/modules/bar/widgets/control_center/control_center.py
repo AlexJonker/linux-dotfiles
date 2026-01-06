e9b89fad0595f12e64955e909cf614f2ff86fe5b
Alex <alex.jonker2008@gmail.com>
2026-01-06 13:52:55 +0100

    Revert ugly corners
     - reverts the ugly corners
     - improves notification popups

--- .config/ignis/modules/bar/widgets/control_center/control_center.py
+++ .config/ignis/modules/bar/widgets/control_center/control_center.py
@@ -8,129 +8,58 @@ from .widgets import (
     Media,
     NotificationCenter,
 )
-from ignis.widgets import Corner
 from .menu import opened_menu
 
 window_manager = WindowManager.get_default()
 
 class ControlCenter(widgets.RevealerWindow):
     def __init__(self):
-        self._tab_index = 0
-
-        # Define tab contents as a list
-        tab_contents = [
-            widgets.Box(vertical=True, child=[User(), QuickSettings(), Brightness()]),
-            widgets.Box(vertical=True, child=[VolumeSlider("speaker"), VolumeSlider("microphone"), Media()]),
-            widgets.Box(vertical=True, child=[NotificationCenter()]),
-        ]
-
-        self.tab_content = widgets.Box(vertical=True, child=[tab_contents[0]])
-
-        def set_tab(idx):
-            self._tab_index = idx
-            self.tab_content.child = [tab_contents[idx]]
-            activate_tab_btn(idx)
-
-        # Create tab buttons in a loop
-        tab_labels = ["General", "Audio", "Notifications"]
-        tab_buttons = []
-        for i, label in enumerate(tab_labels):
-            btn = widgets.Button(
-                child=widgets.Label(label=label),
-                css_classes=["control-center-tab-btn"] + (["active"] if i == 0 else []),
-                on_click=lambda x, idx=i: set_tab(idx),
-            )
-            tab_buttons.append(btn)
-
-        tab_bar = widgets.Box(css_classes=["control-center-tabs"], child=tab_buttons)
-
-        def activate_tab_btn(idx):
-            for i, btn in enumerate(tab_buttons):
-                if i == idx:
-                    btn.add_css_class("active")
-                else:
-                    btn.remove_css_class("active")
-
-        # Main content
-        content = widgets.Box(
-            vertical=True,
-            spacing=8,
-            css_classes=["control-center-box"],
-            child=[
-                widgets.Box(
-                    vertical=True,
-                    css_classes=["control-center"],
-                    child=[tab_bar, self.tab_content],
-                ),
-            ],
-        )
-
-        # Revealer with corners
         revealer = widgets.Revealer(
-            transition_type="slide_down",
+            transition_type="slide_left",
             child=widgets.Box(
-                vertical=False,
+                vertical=True,
+                css_classes=["control-center"],
                 child=[
-                        widgets.Box(
-                            css_classes=["control-center-corner-right"],
-                            halign="start",
-                            valign="start",
-                            child=[Corner(orientation="top-right", width_request=40, height_request=40)],
-                        ),
-                    content,
-                        widgets.Box(
-                            css_classes=["control-center-corner-left"],
-                            halign="start",
-                            valign="start",
-                            child=[Corner(orientation="top-left", width_request=40, height_request=40)],
-                        ),
+                    widgets.Box(
+                        vertical=True,
+                        css_classes=["control-center-widget"],
+                        child=[
+                            QuickSettings(),
+                            VolumeSlider("speaker"),
+                            VolumeSlider("microphone"),
+                            Brightness(),
+                            User(),
+                            Media(),
+                        ],
+                    ),
+                    NotificationCenter(),
                 ],
             ),
             transition_duration=300,
-            reveal_child=False,
+            reveal_child=True,
         )
 
         super().__init__(
             visible=False,
             popup=True,
-            kb_mode="none",
-            layer="overlay",
-            css_classes=["control-window"],
-            anchor=["top", "left", "right", "bottom"],
+            kb_mode="on_demand",
+            layer="top",
+            css_classes=["unset"],
+            anchor=["top", "right", "bottom", "left"],
             namespace="ignis_CONTROL_CENTER",
-            child=widgets.Overlay(
-                child=widgets.Button(
-                    vexpand=True,
-                    hexpand=True,
-                    can_focus=False,
-                    on_click=lambda _: setattr(self, "visible", False),
-                ),
-                overlays=[
-                    widgets.Box(
-                        vertical=True,
-                        valign="start",
-                        halign="center",
-                        style="min-width: 40rem; min-height: 18rem;",
-                        child=[revealer],
-                    )
+            child=widgets.Box(
+                child=[
+                    widgets.Button(
+                        vexpand=True,
+                        hexpand=True,
+                        css_classes=["unset"],
+                        on_click=lambda x: window_manager.close_window("ignis_CONTROL_CENTER"),
+                    ),
+                    revealer,
                 ],
+            ),
+            setup=lambda self: self.connect(
+                "notify::visible", lambda x, y: opened_menu.set_value("")
             ),
             revealer=revealer,
         )
-
-        # Connect visibility change signal
-        self.connect("notify::visible", self.__on_visibility_change, revealer)
-
-    def __on_visibility_change(self, window, param, revealer):
-        for win in getattr(window_manager, "_windows", {}).values():
-            if hasattr(win, "namespace") and str(getattr(win, "namespace", "")).startswith("ignis_BAR_"):
-                if self.visible:
-                    win.add_css_class("control-center-open")
-                else:
-                    win.remove_css_class("control-center-open")
-
-        if self.visible:
-            revealer.reveal_child = True
-        else:
-            opened_menu.set_value("")
-            revealer.reveal_child = False
--- .config/ignis/modules/notification_popup/notification_popup.py
+++ .config/ignis/modules/notification_popup/notification_popup.py
@@ -2,7 +2,6 @@ from ignis import widgets
 from ignis import utils
 from ignis.services.notifications import Notification, NotificationService
 from ..shared_widgets import NotificationWidget
-from ignis.widgets import Corner
 
 notifications = NotificationService.get_default()
 
@@ -17,12 +16,30 @@ class Popup(widgets.Box):
         widget = NotificationWidget(notification)
         widget.css_classes = ["notification-popup"]
 
-        # Inner and outer animations
-        self._inner = widgets.Revealer(transition_type="slide_left", child=widget)
-        self._outer = widgets.Revealer(transition_type="slide_down", child=self._inner)
-        super().__init__(child=[self._outer], halign="end")
+        self._inner = widgets.Revealer(
+            transition_type="slide_left",
+            child=widget,
+            transition_duration=750,
+            reveal_child=False,
+        )
+
+        self._outer = widgets.Revealer(
+            transition_type="slide_down",
+            child=self._inner,
+            transition_duration=750,
+            reveal_child=False,
+        )
+
+        super().__init__(child=[self._outer], halign="start")
 
         notification.connect("dismissed", lambda x: self.destroy())
+
+        self._animate_in()
+
+    def _animate_in(self):
+        self._inner.set_reveal_child(True)
+
+        utils.Timeout(self._inner.transition_duration, lambda: setattr(self._outer, "reveal_child", True))
 
     def destroy(self):
         def box_destroy():
@@ -32,11 +49,11 @@ class Popup(widgets.Box):
 
         def outer_close():
             self._outer.reveal_child = False
-            utils.Timeout(self._outer.transition_duration, box_destroy)
 
         self._inner.transition_type = "slide_right"
         self._inner.reveal_child = False
         utils.Timeout(self._outer.transition_duration, outer_close)
+        utils.Timeout(self._outer.transition_duration, box_destroy)
 
 
 class PopupBox(widgets.Box):
@@ -48,8 +65,7 @@ class PopupBox(widgets.Box):
             vertical=True,
             valign="start",
             setup=lambda self: notifications.connect(
-                "new_popup",
-                lambda x, notification: self.__on_notified(notification),
+                "new_popup", lambda x, notification: self.__on_notified(notification)
             ),
         )
 
@@ -57,101 +73,18 @@ class PopupBox(widgets.Box):
         self._window.visible = True
         popup = Popup(box=self, window=self._window, notification=notification)
         self.prepend(popup)
-        popup._outer.reveal_child = True
-        utils.Timeout(
-            popup._outer.transition_duration, popup._inner.set_reveal_child, True
-        )
 
 
-class NotificationPopup(widgets.RevealerWindow):
+class NotificationPopup(widgets.Window):
     def __init__(self, monitor: int):
-        self.popup_box = PopupBox(window=self, monitor=monitor)
-
-        # Notification content box
-        content = widgets.Box(
-            vertical=True,
-            spacing=8,
-            css_classes=["notification-popup-box"],
-            child=[self.popup_box],
-        )
-
-        # Corners
-        content_with_corners = widgets.Box(
-            vertical=True,
-            child=[
-                widgets.Box(
-                    halign="end",
-                    valign="end",
-                    css_classes=["notification-popup-corner-up"],
-                    child=[
-                        Corner(
-                            orientation="bottom-right",
-                            width_request=40,
-                            height_request=40,
-                        )
-                    ],
-                ),
-                content,
-                widgets.Box(
-                    halign="end",
-                    valign="end",
-                    css_classes=["notification-popup-corner-down"],
-                    child=[
-                        Corner(
-                            orientation="top-right",
-                            width_request=40,
-                            height_request=40,
-                        )
-                    ],
-                ),
-            ],
-        )
-
-        # Content revealer
-        self.content_revealer = widgets.Revealer(
-            transition_type="slide_left",
-            child=content_with_corners,
-            transition_duration=300,
-            reveal_child=False,
-        )
-
-        # Dismiss buttons
-        start_dismiss = widgets.Button(
-            vexpand=True,
-            hexpand=True,
-            css_classes=["notification-popup-dismiss"],
-            on_click=lambda x: setattr(self, "visible", False),
-        )
-        end_dismiss = widgets.Button(
-            vexpand=True,
-            hexpand=True,
-            css_classes=["notification-popup-dismiss"],
-            on_click=lambda x: setattr(self, "visible", False),
-        )
-
         super().__init__(
-            visible=False,
-            popup=True,
-            kb_mode="none",
-            layer="overlay",
-            css_classes=["notification-popup-window"],
-            anchor=["top", "bottom", "right"],
+            anchor=["left", "top", "bottom"],
             monitor=monitor,
             namespace=f"ignis_NOTIFICATION_POPUP_{monitor}",
-            child=widgets.CenterBox(
-                hexpand=True,
-                halign="fill",
-                vexpand=True,
-                vertical=True,
-                start_widget=start_dismiss,
-                center_widget=widgets.Box(
-                    vertical=True,
-                    valign="center",
-                    halign="end",
-                    child=[self.content_revealer],
-                ),
-                end_widget=end_dismiss,
-            ),
-            revealer=self.content_revealer,
+            layer="top",
+            child=PopupBox(window=self, monitor=monitor),
+            visible=False,
             dynamic_input_region=True,
+            css_classes=["rec-unset"],
+            style="min-width: 29rem;",
         )
