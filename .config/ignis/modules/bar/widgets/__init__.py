3c4b19bf45e31fb8ecb013759ede8be201cfc9f4
Alex <81556756+AlexJonker@users.noreply.github.com>
2026-01-10 01:33:11 +0100

    Delete .config/ignis directory
    Won't be working on this for hyprland so bye bye

--- .config/ignis/modules/bar/widgets/__init__.py
+++
@@ -1,9 +0,0 @@
-from .pill import StatusPill
-from .tray import Tray
-from .battery import Battery
-from .apps import Apps
-from .workspaces import Workspaces
-from .media import Media
-from .tasklist import TaskList
-
-__all__ = ["StatusPill", "Tray", "Battery", "Apps", "Workspaces", "Media", "TaskList"]
